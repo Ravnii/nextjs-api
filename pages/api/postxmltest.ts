a66919e667c7b5068e57f03b0bde85a9cdbc6ece
Jesper Ravn <jeper.ravn@hotmail.com>
2023-11-07 13:42:07 +0100

    test readable

--- pages/api/postxmltest.ts
+++ pages/api/postxmltest.ts
@@ -1,10 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const postxmltest = (_: NextApiRequest, res: NextApiResponse) => {
-  const {method, body} = _;
-
   const url = 'https://postxmltest.blob.core.windows.net/myblobcontainer/myblockblob/myendpointblob.xml?sp=racwdli&st=2023-11-07T07:29:35Z&se=2023-11-07T15:29:35Z&spr=https&sv=2022-11-02&sr=c&sig=am1UEUIlcqpWg80w%2FBBb3nabkxCPq8sXxA6xmckR%2BFo%3D'
-
+  const body = _.body;
+  
   fetch(url, {
     method: 'PUT',
     headers: {
@@ -13,10 +12,34 @@ const postxmltest = (_: NextApiRequest, res: NextApiResponse) => {
     },
     body: `Called from M3
     body-length: ${body.length}
-    body-content: ${body}`
+    body-content: ${body}
+    readable: ${_.readable}
+    errored: ${_.errored}`
   }).then((result) => {
     res.status(200).json(result)
   });
+/*
+  const chunks = [];
+
+  _.on('readable', () => {
+    let chunk;
+    while (null !== (chunk = _.read())) {
+      chunks.push(chunk);
+    }
+  });
+  
+  _.on('end', () => {
+    const content = chunks.join('');
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'x-ms-version': '2023-11-03',
+        'x-ms-blob-type': 'BlockBlob'
+      },
+      body: content,
+    })
+  });
+  */
 }
 
 export default postxmltest;
