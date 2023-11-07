fcf24355d1b15aa7be9f0e34bad52575a3fb2441
Jesper Ravn <jeper.ravn@hotmail.com>
2023-11-07 08:32:43 +0100

    update url

--- pages/api/postxmltest.ts
+++ pages/api/postxmltest.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const postxmltest = (_: NextApiRequest, res: NextApiResponse) => {
   const {method, body} = _;
 
-  const url = 'https://postxmltest.blob.core.windows.net/myblobcontainer/myblockblob/myblob.xml?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-11-06T18:52:01Z&st=2023-11-06T10:52:01Z&spr=https&sig=TDGRx4FpPmp%2BWzTwz5iXM1VSnLS0qNiE9QshrkNyVFM%3D'
+  const url = 'https://postxmltest.blob.core.windows.net/myblobcontainer/myblockblob/myendpointblob.xml?sp=racwdli&st=2023-11-07T07:29:35Z&se=2023-11-07T15:29:35Z&spr=https&sv=2022-11-02&sr=c&sig=am1UEUIlcqpWg80w%2FBBb3nabkxCPq8sXxA6xmckR%2BFo%3D'
 
   fetch(url, {
     method: 'PUT',
@@ -11,7 +11,7 @@ const postxmltest = (_: NextApiRequest, res: NextApiResponse) => {
       'x-ms-version': '2023-11-03',
       'x-ms-blob-type': 'BlockBlob'
     },
-    body: body
+    body: 'Called from M3'
   });
 
   // https://postxmltest.blob.core.windows.net/?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-11-06T18:52:01Z&st=2023-11-06T10:52:01Z&spr=https&sig=TDGRx4FpPmp%2BWzTwz5iXM1VSnLS0qNiE9QshrkNyVFM%3D
