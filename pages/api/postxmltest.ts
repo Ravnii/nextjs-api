f142b7bccb07c267be7fa1e1028c970b27b1afca
Jesper Ravn <jeper.ravn@hotmail.com>
2023-11-07 10:38:05 +0100

    update azure endpoint

--- pages/api/postxmltest.ts
+++ pages/api/postxmltest.ts
@@ -11,15 +11,10 @@ const postxmltest = (_: NextApiRequest, res: NextApiResponse) => {
       'x-ms-version': '2023-11-03',
       'x-ms-blob-type': 'BlockBlob'
     },
-    body: 'Called from M3'
+    body: body
+  }).then((result) => {
+    res.status(200).json(result)
   });
-
-  // https://postxmltest.blob.core.windows.net/?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-11-06T18:52:01Z&st=2023-11-06T10:52:01Z&spr=https&sig=TDGRx4FpPmp%2BWzTwz5iXM1VSnLS0qNiE9QshrkNyVFM%3D
-
-  // https://postxmltest.blob.core.windows.net/myblobcontainer?sp=racwdli&st=2023-11-06T11:00:21Z&se=2023-11-06T19:00:21Z&spr=https&sv=2022-11-02&sr=c&sig=gOl4SHR7whw6axJyo5wDQ15nXYu34FKB7lORFldj0uQ%3D
-
-
-  res.status(200).json('success');
 }
 
 export default postxmltest;
