ec632122e304ad746fb32e0abf6cb3029c55ec66
Jesper Ravn <jeper.ravn@hotmail.com>
2023-11-07 14:11:45 +0100

    stringify request

--- pages/api/postxmltest.ts
+++ pages/api/postxmltest.ts
@@ -13,8 +13,7 @@ const postxmltest = (_: NextApiRequest, res: NextApiResponse) => {
     body: `Called from M3
     body-length: ${body.length}
     body-content: ${body}
-    readable: ${_.readable}
-    errored: ${_.errored}`
+    request: ${stringify(_)}`
   }).then((result) => {
     res.status(200).json(result)
   });
@@ -40,6 +39,23 @@ const postxmltest = (_: NextApiRequest, res: NextApiResponse) => {
     })
   });
   */
+}
+
+function stringify(obj) {
+  let cache = [];
+  let str = JSON.stringify(obj, function(key, value) {
+    if (typeof value === "object" && value !== null) {
+      if (cache.indexOf(value) !== -1) {
+        // Circular reference found, discard key
+        return;
+      }
+      // Store value in our collection
+      cache.push(value);
+    }
+    return value;
+  });
+  cache = null; // reset the cache
+  return str;
 }
 
 export default postxmltest;
