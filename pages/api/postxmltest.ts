fc2645dcf55274599f509820717d524637404bb5
Jesper Ravn <jspr.ravn@gmail.com>
2023-11-09 10:32:43 +0100

    Update postxmltest.ts

--- pages/api/postxmltest.ts
+++ pages/api/postxmltest.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const postxmltest = (_: NextApiRequest, res: NextApiResponse) => {
-  const url = 'https://postxmltest.blob.core.windows.net/myblobcontainer/myblockblob/myendpointblob.xml?sp=racwdli&st=2023-11-07T07:29:35Z&se=2023-11-07T15:29:35Z&spr=https&sv=2022-11-02&sr=c&sig=am1UEUIlcqpWg80w%2FBBb3nabkxCPq8sXxA6xmckR%2BFo%3D'
+  const url = 'https://postxmltest.blob.core.windows.net/myblobcontainer/myblockblob/myendpointblob.xml?sp=racwdli&st=2023-11-09T09:31:53Z&se=2023-11-09T17:31:53Z&sv=2022-11-02&sr=c&sig=8lUNXkiJOXkcd4AkXMtihQJlaXwUhC0PCCKgEivCWtM%3D'
   const body = _.body;
 
   fetch(url, {
